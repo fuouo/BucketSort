a32775d7b8a6e302cedb5db95c4434d97497fc2d
fuouo <fuouokun@gmail.com>
2017-06-20 01:01:11 +0800

    Workign BucketSort v2
    +Fixed divider and assignedBuckets
    - using arraylist

--- src/BucketSort.java
+++ src/BucketSort.java
@@ -4,28 +4,26 @@ public class BucketSort {
 	
 	InsertionSort is = new InsertionSort();
 	
-	public int[] doBucketSort(int[] arr){
-		//step 1: Get size of Array
-		//Step 2: Get max value
-		//Step 3: Get amount of Buckets
-		//Step 4: Get Divider = ceil( (max+1) / bucket size)
-		//Step 5: Assigning to Buckets: assigned_bucket = floor(A[i] / divider)
-		//Step 6: Insetion Sort every buckets
-		//Step 7: Merge bucket
-		
+	/*
+	step 1: Get size of Array
+	Step 2: Get max value
+	Step 3: Get amount of Buckets
+	Step 4: Get Divider = ceil( (max+1) / bucket size)
+	Step 5: Assigning to Buckets: assigned_bucket = floor(A[i] / divider)
+	Step 6: Insetion Sort every buckets
+	Step 7: Merge bucket
+	*/
+	
+	public int[] doBucketSort(int[] arr){	
 		int size = arr.length; //step 1
 		int max = getMax(arr); //step 2
 		ArrayList<Integer>[] bucket = (ArrayList<Integer>[]) new ArrayList[10]; //step 3
-		int divider = (int) Math.ceil((float)(max+1)/bucket.length); //step 4
-		
-		//TODO: Divider doesn't work for 2 digit numbers???
-		System.out.println("Max: " + max + " || Divider " + divider);
+		int divider = String.valueOf(max).length();
 
 		//step 5
 		int assignedBucket = 0;
 		for(int i=0; i<arr.length; i++){
-			assignedBucket = (int)Math.floor(arr[i]/divider);
-			System.out.println("Number : " + arr[i] + " || Assigned Bucket : " + assignedBucket + " || Divider: " + divider);
+			assignedBucket = (int) (arr[i] / Math.pow(10, divider-1));
 			if(bucket[assignedBucket] == null){
 				bucket[assignedBucket] = new ArrayList<Integer>();
 			}
@@ -34,12 +32,15 @@ public class BucketSort {
 		
 		//step 6
 		for(int i=0; i<bucket.length; i++){
-			System.out.print("Bucket [" + i + "]: ");
 			if(bucket[i] != null && bucket[i].size() != 0){
+				System.out.print("Bucket [" + i + "]: ");
 				bucket[i] = is.doInsertionSort(bucket[i]);
 				for(int j=0; j<bucket[i].size(); j++){
 					System.out.print(bucket[i].get(j)+ " ");
 				}
+			}
+			else{
+				continue;
 			}
 			System.out.println();
 		}
@@ -50,10 +51,12 @@ public class BucketSort {
 	
 	//TODO: Wrong Conversion to Array code. Under construction.
 	public int[] convertToArr(ArrayList<Integer>[] bucket, int size){
-		int[] output = new int[bucket.length];
+		int[] output = new int[size];
+		int index = 0;
 		for(int i=0; i<bucket.length; i++){
 			for(int j=0; bucket[i] != null && j< bucket[i].size(); j++){
-				output[i] = bucket[i].get(j);
+				output[index] = bucket[i].get(j);
+				index ++;
 			}
 		}
 		return output;
